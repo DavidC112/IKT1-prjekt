84b1f5f629ed1d00639ec934c3019dc26f7c9898
Cornuta David <cornutadavid0@gmail.com>
2023-09-14 09:04:28 +0200

    1. commit

--- veletlen.py
+++ veletlen.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import Random 
+
+sz_also=int(input("Mennyi legyen a számok generálásának az alsó határa: "))
+sz_felso=int(input("Mennyi legyen a számok generálásának az felső határa: "))
+sz_db=int(input("Mennyi szám legyen generálva: "))
+for in in range(sz_db)
+{
+    
+}
